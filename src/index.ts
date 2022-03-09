451df62324af630af9342afa920c32110e5be148
ABDELKAWY A. ABDELAZIZ <abdelkawy.abdelhady@gmail.com>
2022-03-09 22:53:36 +0400

    express installed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!!");
+import express, {Application, Request, Response} from "express";
+
+const app: Application = express();
+
+// define a port
+const PORT = 3003;
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Welcome to express",
+  });
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log("listening to 3003");
+});
+
+export default app;
